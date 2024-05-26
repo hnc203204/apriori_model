b6fd40fa69898578e588cd70062b551228b45dea
Cong Huynh <hnc203204@gmail.com>
2024-05-26 10:31:31 +0700

    fix

--- ecommerce_website/__init__.py
+++ ecommerce_website/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-itemsets, rules = apriori_mlxtend(os.path.realpath(".") + '/data_processing/transactions.csv', min_support=0.001, min_confidence=1.0)
+itemsets, rules = apriori_mlxtend(os.path.realpath(".") + '/ecommerce_website/data/transactions.csv', min_support=0.001, min_confidence=1.0)
 
 from ecommerce_website.views.submit_basket import submit_basket
 
--- ecommerce_website/apriori_algo/aprori.py
+++ ecommerce_website/apriori_algo/aprori.py
@@ -8,7 +8,9 @@ def apriori_mlxtend(
         min_confidence: float = 1.0
 ):
     df = pd.read_csv(df_path)
+
     df.drop("Unnamed: 0", axis=1, inplace=True)
+    df.drop("Unnamed: 0.1", axis=1, inplace=True)
     frequent_itemsets = mlxtend_apriori(df, min_support=min_support, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=min_confidence)
     return frequent_itemsets, rules
